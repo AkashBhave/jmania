fdb404892913d3d91d2e85259919216d9e6950fe
acidepicice <38502391+acidepicice@users.noreply.github.com>
2018-05-02 11:58:16 -0400

    Add layout, buttons

--- panels/PanelHome.java
+++ panels/PanelHome.java
@@ -3,20 +3,47 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelHome extends JPanel {
+        
+        private int width;
+        private int height;
 
-    
-    public void paintComponent (Graphics g) {
-    
-        ImageIcon logo = new ImageIcon(System.getProperty("user.dir") + "/jmania/assets/logoBanner.png");
-        g.setColor(new Color(240, 255, 240));
-        g.fillRect(0, 0, 1280, 720);
-        g.drawImage(logo.getImage(), 320, 50, 600, 300, null, null);
+        public PanelHome (int width, int height) {
         
-        JButton play = new JButton ("Play");
+            this.width = width;
+            this.height = height;
         
-        JButton settings = new JButton ("Settings");
+            setLayout(new FlowLayout(FlowLayout.CENTER, 0, 450));
+            JPanel buttons = new JPanel();
+            buttons.setOpaque(false);
+            buttons.setLayout(new FlowLayout(FlowLayout.CENTER, 75, 0));
+            add(buttons);
+            
+            Dimension buttonDimension = new Dimension(120, 45);
+            Font buttonFont = new Font("Courier New", Font.PLAIN, 17);
+
+            JButton settings = new JButton ("Settings");
+            settings.setPreferredSize(buttonDimension);
+            settings.setFont(buttonFont);
+            buttons.add(settings);
+            
+            JButton play = new JButton ("Play");
+            play.setPreferredSize(buttonDimension);
+            play.setFont(buttonFont);
+            buttons.add(play);
+            
+            JButton credits = new JButton ("Credits");
+            credits.setPreferredSize(buttonDimension);
+            credits.setFont(buttonFont);
+            buttons.add(credits);
+
+        }
         
-        JButton credits = new JButton ("Credits");
+        public void paintComponent(Graphics g) {
         
-    }
+            ImageIcon logo = new ImageIcon(System.getProperty("user.dir") + "/jmania/assets/logoBanner.png");
+            g.setColor(new Color(240, 255, 240));
+            g.fillRect(0, 0, width, height);
+            g.drawImage(logo.getImage(), 320, 100, 600, 300, null, null);
+            
+        }
 }
