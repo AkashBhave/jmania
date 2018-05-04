d86f5506cc7b6ce7847a2e7f0b623e0a260fdf42
acidepicice <38502391+acidepicice@users.noreply.github.com>
2018-05-04 11:52:27 -0400

    Add Listeners

--- panels/PanelHome.java
+++ panels/PanelHome.java
@@ -1,6 +1,7 @@
 package jmania.panels;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class PanelHome extends JPanel {
         
@@ -11,7 +12,7 @@ public class PanelHome extends JPanel {
         
             this.width = width;
             this.height = height;
-        
+                
             setLayout(new FlowLayout(FlowLayout.CENTER, 0, 450));
             JPanel buttons = new JPanel();
             buttons.setOpaque(false);
@@ -22,16 +23,22 @@ public class PanelHome extends JPanel {
             Font buttonFont = new Font("Courier New", Font.PLAIN, 17);
 
             JButton settings = new JButton ("Settings");
+            settings.setFocusPainted(false);
+            settings.addActionListener(new Settings());
             settings.setPreferredSize(buttonDimension);
             settings.setFont(buttonFont);
             buttons.add(settings);
             
             JButton play = new JButton ("Play");
+            play.setFocusPainted(false);
+            play.addActionListener(new Play());
             play.setPreferredSize(buttonDimension);
             play.setFont(buttonFont);
             buttons.add(play);
             
             JButton credits = new JButton ("Credits");
+            credits.setFocusPainted(false);
+            credits.addActionListener(new Credits());
             credits.setPreferredSize(buttonDimension);
             credits.setFont(buttonFont);
             buttons.add(credits);
@@ -45,5 +52,36 @@ public class PanelHome extends JPanel {
             g.fillRect(0, 0, width, height);
             g.drawImage(logo.getImage(), 320, 100, 600, 300, null, null);
             
+        }
+        
+        private class Play implements ActionListener {
+        
+            public void actionPerformed (ActionEvent e) {
+               
+                System.out.println("Play");
+                                
+            }
+        
+        }
+        
+        private class Settings implements ActionListener {
+        
+            public void actionPerformed (ActionEvent e) {
+            
+                System.out.println("Settings");
+                
+            
+            }
+
+        }
+        
+        private class Credits implements ActionListener {
+        
+            public void actionPerformed (ActionEvent e) {
+            
+                System.out.println("Credits");    
+            
+            }
+
         }
 }
