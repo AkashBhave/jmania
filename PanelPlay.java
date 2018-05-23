0acf9dd3134aba6a2c1aeb6d0eddce0777d5e966
Avik Rao <38502391+acidepicice@users.noreply.github.com>
2018-05-23 08:30:26 -0400

    Implement rhythm synchronization

--- PanelPlay.java
+++ PanelPlay.java
@@ -1,10 +1,37 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.net.URL;
+import java.text.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import javax.sound.sampled.*;
+import javax.swing.*;
 
 @SuppressWarnings("serial")
+
+class RunAudio implements Runnable 
+{
+   private Clip music;
+   
+   public RunAudio(Clip music)
+   {
+      this.music = music;
+   }
+   public void run()
+   {
+      if (!music.isRunning())
+         music.start();
+   }
+}
+
 public class PanelPlay extends JPanel implements ActionListener {
 
+    private int arraySize = 1000;
     private int width, height; 
     private int[] x = new int[1000]; // list of arrow x-coords
     private int[] y = new int[1000]; // list of arrow y-coords
@@ -14,26 +41,64 @@ public class PanelPlay extends JPanel implements ActionListener {
     private Timer tm = new Timer(1, this); // animation timer
     
     // some ints to make life easier
-    private final int left = 0;
-    private final int down = 1;
-    private final int up = 2;
-    private final int right = 3;
-    private final int notPressed = 4;
+    private final String left = "1000";
+    private final String down = "0100";
+    private final String up = "0010";
+    private final String right = "0001";
+    private final String notPressed = "0000";
+
+    private long temptime;
+
+    private long milltime = System.currentTimeMillis();;
+    private boolean songstarted = false;
 
     private int arrowCount = 0; // number of arrows initialized
-    private int nextArrow = 1; // the topmost arrow to be destroyed
+    private int nextArrow; // the topmost arrow to be destroyed
     private int timeCount; // counter for when to initialize a new arrow
     private int newArrowTime = 225; // when timecount reaches this (this variable's value * timer's delay = time in ms) initialize a new arrow
-    private int pressed = 4; // set to the value of the arrow key that is pressed, used to check which arrow should be destroyed
+    private String pressed = notPressed; // set to the value of the arrow key that is pressed, used to check which arrow should be destroyed
     private ImageIcon inactiveLeft, inactiveDown, inactiveUp, inactiveRight; // default arrow sprites
     private ImageIcon active[] = new ImageIcon[1000]; // list of all the arrows
+
+    public Simfile smf = new Simfile("assets/songs/Never_Gonna_Give_You_Up/Never_Gonna_Give_You_Up.csm", 0.0);
+    List<List<String>> timestamp = smf.NotesTime();
+    public int noteindex = 0;
+    public File file;
+    public Clip music;
+
+    public int addnoteindex() // literally just for the actionlistener
+    {
+        noteindex++;
+        return 1;
+    }
     
-    public PanelPlay (Window owner, int width, int height, Simfile simfile) {
+    public PanelPlay (Window owner, int width, int height, Simfile simfile) throws Exception {
+        try { 
+            this.file = new File("assets/songs/Never_Gonna_Give_You_Up/Never_Gonna_Give_You_Up.wav");
+            if (file.exists()) {
+                AudioInputStream stream = AudioSystem.getAudioInputStream(file);
+                this.music = AudioSystem.getClip();
+                music.open(stream);
+            } 
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException("There was an I/O error reading the audio file.");
+        }
+        catch (LineUnavailableException e)
+        {
+            throw new RuntimeException("We messed up, no line is available for the sound.");
+        }
+        catch (UnsupportedAudioFileException e)
+        {
+            throw new RuntimeException("This type of audio file is unsupported. Only uncompressed formats are supported.");
+        }
+
         this.owner = owner;
         this.width = width;
         this.height = height;
 
-        y[arrowCount] = 800; // starts first arrow at y-coordinate 800 (80 pixels below bottom of screen)
+        y[noteindex] = 800; // starts first arrow at y-coordinate 800 (80 pixels below bottom of screen)
         
         // key bindings
         int arrowMap = JComponent.WHEN_IN_FOCUSED_WINDOW;
@@ -53,15 +118,16 @@ public class PanelPlay extends JPanel implements ActionListener {
         amap.put("loadDown", new ArrowAction(down));
         amap.put("loadUp", new ArrowAction(up));
         
+        
         owner.requestFocus();
     }
     
     // gets called when arrow key is pressed, parameter depends on which arrow key
     private class ArrowAction extends AbstractAction {
         
-        private int direction;
+        private String direction;
         
-        public ArrowAction (int direction) {
+        public ArrowAction (String direction) {
             this.direction = direction;
         }
         
@@ -77,22 +143,27 @@ public class PanelPlay extends JPanel implements ActionListener {
 
         // if (value of newArrowTime * value of timer's delay) ms has passes, initialize a new arrow randomly
         timeCount++;
-        if (timeCount % newArrowTime == 0) {
+        /*if (timeCount % newArrowTime == 0) {
             int random = (int)(Math.random() * 4);
             arrowCount += 1;
             loadArrow(random);
             y[arrowCount] = 800;
-        }
+        }*/
 
         // animate all existing arrows
-        for (int start = arrowCount; start > 0; start --) {
+        for (int start = noteindex; start > 0; start --) {
             y[start] -= velY;
+            if (y[start] <= -80 && active[start] != null) {
+                active[start] = null;
+                System.out.println("rekt");
+            }
         }   
 
-        // if an arrow goes off the screen, destroy it
-        if (y[nextArrow] < -80 && y[nextArrow] != 0) {
-            active[nextArrow] = null;
-            nextArrow ++;
+        for (int start = 1; start < noteindex; start ++) {
+            if (active[start] != null) {
+                nextArrow = start;
+                break;
+            }
         }
 
         // if an arrow key is pressed and the topmost arrow is the corresponding arrow, destroy it.
@@ -128,8 +199,34 @@ public class PanelPlay extends JPanel implements ActionListener {
             }
             pressed = notPressed;
         }
+        temptime = System.currentTimeMillis();
 
-        repaint(); // redraw the canvas after all is said and done
+
+
+        if (temptime-milltime >= 16) {
+            repaint();
+            milltime = temptime;
+        }
+
+         // redraw the canvas after all is said and done
+        
+        if (!songstarted) {
+            new Thread(new RunAudio(music)).start(); 
+            songstarted = true;
+        }
+
+        double micro = (double)music.getMicrosecondPosition();
+        double second = micro/1000000;
+        //label1.setText(Double.toString(micro));
+        if (second > Double.parseDouble(timestamp.get(noteindex).get(0))-0.7)
+        {
+            loadArrow(timestamp.get(noteindex).get(1));
+            y[noteindex] = 800;
+            
+            addnoteindex();
+            //System.out.println(Double.toString(second));
+            //System.out.println(timestamp.get(noteindex).get(0));
+        }
 
     }
     
@@ -144,10 +241,10 @@ public class PanelPlay extends JPanel implements ActionListener {
         loadStaticSprites(g); // draw default arrows
         
         // redraw every arrow with their updated y-coord values (the real animation)
-        if (arrowCount >= 1) {
-            for (int start = arrowCount; start > 0; start --) {
+        if (noteindex >= 1) {
+            for (int start = noteindex; start > 0; start --) {
                 if (active[start] != null) {
-                    g.drawImage(active[start].getImage(), x[start], y[start], 80, 80, null, null);
+                    g.drawImage(active[start].getImage(), x[start], y[start], null, null);
                 }
             }
         }
@@ -163,39 +260,42 @@ public class PanelPlay extends JPanel implements ActionListener {
         inactiveUp = new ImageIcon(arrowPath + "inactiveUp.png");
         inactiveRight = new ImageIcon(arrowPath + "inactiveRight.png");
         
-        g.drawImage(inactiveLeft.getImage(), 425, 100, 80, 80, null, null);
-        g.drawImage(inactiveDown.getImage(), 525, 100, 80, 80, null, null);
-        g.drawImage(inactiveUp.getImage(), 625, 100, 80, 80, null, null);
-        g.drawImage(inactiveRight.getImage(), 725, 100, 80, 80, null, null);
+        g.drawImage(inactiveLeft.getImage(), 425, 100, null, null);
+        g.drawImage(inactiveDown.getImage(), 525, 100, null, null);
+        g.drawImage(inactiveUp.getImage(), 625, 100, null, null);
+        g.drawImage(inactiveRight.getImage(), 725, 100, null, null);
         
     }
     
     // make new arrows 
-    private void loadArrow (int direction) {
+    private void loadArrow (String direction) {
         switch (direction) {
             
-            case 0 :
-            active[arrowCount] = new ImageIcon(arrowPath + "activeLeft.png");
-            active[arrowCount].setDescription("left");
-            x[arrowCount] = 425;
+            case left :
+            active[noteindex] = new ImageIcon(arrowPath + "activeLeft.png");
+            active[noteindex].setDescription("left");
+            x[noteindex] = 425;
             break;
             
-            case 1 :
-            active[arrowCount] = new ImageIcon(arrowPath + "activeDown.png");
-            active[arrowCount].setDescription("down");
-            x[arrowCount] = 525;
+            case down :
+            active[noteindex] = new ImageIcon(arrowPath + "activeDown.png");
+            active[noteindex].setDescription("down");
+            x[noteindex] = 525;
             break;
             
-            case 2 :
-            active[arrowCount] = new ImageIcon(arrowPath + "activeUp.png");
-            active[arrowCount].setDescription("up");
-            x[arrowCount] = 625;
+            case up :
+            active[noteindex] = new ImageIcon(arrowPath + "activeUp.png");
+            active[noteindex].setDescription("up");
+            x[noteindex] = 625;
             break;
             
-            case 3 :
-            active[arrowCount] = new ImageIcon(arrowPath + "activeRight.png");
-            active[arrowCount].setDescription("right");
-            x[arrowCount] = 725;
+            case right :
+            active[noteindex] = new ImageIcon(arrowPath + "activeRight.png");
+            active[noteindex].setDescription("right");
+            x[noteindex] = 725;
+            break;
+
+            case notPressed :
             break;
 
         }
