622e48cf27d05e18ab1f4ef0d709fb735e87c207
BedrockSolid <bedrocksolidmc@gmail.com>
2018-05-31 20:38:27 -0400

    Complete Javadoc commenting for Simfile class

--- Simfile.java
+++ Simfile.java
@@ -16,26 +16,74 @@ import java.util.List;
 
 public class Simfile {
 
+    /**
+     * The filename of the simfile being accessed.
+     */
     public String filename;
 
+    /**
+     * The object which contains the simfile.
+     */
     private BufferedReader sf;
-    private List < String > simlist = new ArrayList < String > ();
+
+    /**
+     * A list of Strings containing each line in the simfile.
+     */
+    private List<String> simlist = new ArrayList<String>();
+
+    /**
+     * An array of Strings containing each line in the simfile. This is the variable used in methods outside the constructor.
+     */
     private String[] sim;
 
+    /**
+     * An array of Strings containing each BPM in the simfile along with the time in beats in which they occur.
+     */
     private String[] bpms;
 
+    /**
+     * A variable used to temporarily store each line to put in the simlist array.
+     */
     private String line;
+
+    /**
+     * A string containing all the notes in a simfile.
+     */
     private String notestr;
-    private List < String > noteslist = new ArrayList < String > ();
-    private List < List < String >> note = new ArrayList < List < String >> ();
+    
+    /**
+     * A list of strings containing all the notes in a simfile, split by beat.
+     */
+    private List<String> noteslist = new ArrayList<String>();
 
-    private List < List < String >> note1 = new ArrayList < List < String >> ();
+    /**
+     * A list of lists of strings containing all the notes in a simfile in strings, split by beat.
+     */
+    private List<List<String>> note = new ArrayList<List<String>>();
 
+    /**
+     * An array of Strings containing each BPM in the simfile along with the time in beats in which they occur.
+     */
+    private List<List<String>> note1 = new ArrayList<List<String>>();
+
+    /**
+     * An array of doubles containing each BPM in the simfile along with the time in beats in which they occur.
+     */
     private double[][] a;
+
+    /**
+     * This matches the notes into a regular expression which splits the series of notes into groups of four numbers.
+     */
     private Matcher m;
 
+    /**
+     * A double indicating how delayed the arrows should be in comparison to any song being played.
+     */
     private double globaloffset;
 
+    /**
+     * A double indicating how delayed the arrows should be in comparison to the specified song in the simfile.
+     */
     private double offset;
 
     /**
@@ -45,8 +93,7 @@ public class Simfile {
     public Simfile(String filename, double globaloffset) throws Exception {
         this.globaloffset = globaloffset;
         sf = new BufferedReader(new FileReader(filename));
-        while ((line = sf.readLine()) != null) // takes file and put each line into a list
-        {
+        while ((line = sf.readLine()) != null) { // takes file and put each line into a list
             simlist.add(line);
         }
         sim = simlist.toArray(new String[0]); // converts mutable list into immutable array
@@ -58,8 +105,7 @@ public class Simfile {
      */
     public String Title() {
         for (int i = 0; i < sim.length; i++) {
-            if (sim[i].contains("#TITLE:")) // searches each line for title, then strips line of unnecessary characters
-            {
+            if (sim[i].contains("#TITLE:")) { // searches each line for title, then strips line of unnecessary characters
                 return sim[i].replaceAll("#TITLE:", "").replaceAll("\n", "").replaceAll(";", "");
             }
         }
@@ -73,8 +119,7 @@ public class Simfile {
      */
     public String Subtitle() {
         for (int i = 0; i < sim.length; i++) {
-            if (sim[i].contains("#SUBTITLE:")) // searches each line for subtitle, then strips line of unnecessary characters
-            {
+            if (sim[i].contains("#SUBTITLE:")) { // searches each line for subtitle, then strips line of unnecessary characters
                 return sim[i].replaceAll("#SUBTITLE:", "").replaceAll("\n", "").replaceAll(";", "");
             }
         }
@@ -87,8 +132,7 @@ public class Simfile {
      */
     public String Artist() {
         for (int i = 0; i < sim.length; i++) {
-            if (sim[i].contains("#ARTIST:")) // searches each line for artist, then strips line of unnecessary characters
-            {
+            if (sim[i].contains("#ARTIST:")) { // searches each line for artist, then strips line of unnecessary characters
                 return sim[i].replaceAll("#ARTIST:", "").replaceAll("\n", "").replaceAll(";", "");
             }
         }
@@ -101,8 +145,7 @@ public class Simfile {
      */
     public String Genre() {
         for (int i = 0; i < sim.length; i++) {
-            if (sim[i].contains("#GENRE:")) // searches each line for genre, then strips line of unnecessary characters
-            {
+            if (sim[i].contains("#GENRE:")) { // searches each line for genre, then strips line of unnecessary characters
                 return sim[i].replaceAll("#GENRE:", "").replaceAll("\n", "").replaceAll(";", "");
             }
         }
@@ -115,8 +158,7 @@ public class Simfile {
      */
     public int Year() {
         for (int i = 0; i < sim.length; i++) {
-            if (sim[i].contains("#YEAR:")) // searches each line for year, then strips line of unnecessary characters
-            {
+            if (sim[i].contains("#YEAR:")) { // searches each line for year, then strips line of unnecessary characters
                 return Integer.parseInt(sim[i].replaceAll("#YEAR:", "").replaceAll("\n", "").replaceAll(";", ""));
             }
         }
@@ -129,8 +171,7 @@ public class Simfile {
      */
     public String AudioFile() {
         for (int i = 0; i < sim.length; i++) {
-            if (sim[i].contains("#MUSIC:")) // searches each line for audio file, then strips line of unnecessary characters and adds directory
-            {
+            if (sim[i].contains("#MUSIC:")) { // searches each line for audio file, then strips line of unnecessary characters and adds directory
                 String temp = sim[i].replaceAll("#MUSIC:", "").replaceAll("\n", "").replaceAll(";", "");
                 return ("assets/songs/" + (temp.substring(0, temp.length() - 4)) + "/" + temp);
             }
@@ -145,8 +186,7 @@ public class Simfile {
      */
     public double[][] BPM() {
         for (int i = 0; i < sim.length; i++) {
-            if (sim[i].contains("#BPMS:")) // searches each line for BPM, then strips line of unnecessary characters and splits values into arrays
-            {
+            if (sim[i].contains("#BPMS:")) { // searches each line for BPM, then strips line of unnecessary characters and splits values into arrays
                 bpms = sim[i].replaceAll("#BPMS:", "").replaceAll("\n", "").replaceAll(";", "").split(",");
                 double[][] bpmarray = new double[bpms.length][2];
                 for (int a = 0; a < bpms.length; a++) {
@@ -166,8 +206,7 @@ public class Simfile {
      */
     public double Offset() {
         for (int i = 0; i < sim.length; i++) {
-            if (sim[i].contains("#OFFSET:")) // searches each line for offset, then strips line of unnecessary characters
-            {
+            if (sim[i].contains("#OFFSET:")) { // searches each line for offset, then strips line of unnecessary characters
                 return Double.parseDouble(sim[i].replaceAll("#OFFSET:", "").replaceAll("\n", "").replaceAll(";", ""));
             }
         }
@@ -181,8 +220,8 @@ public class Simfile {
      * Each list of strings inside the main list is split by beat, meaning that if there are 8 strings in an array, the notes are split into 1/8 beats.
      * If there are no notes in the simfile, a blank array will be returned.
      */
-    public List < List < String >> Notes() {
-        note = new ArrayList < List < String >> ();
+    public List<List<String>> Notes() {
+        note = new ArrayList<List<String>>();
         for (int i = 0; i < sim.length; i++) {
             if (sim[i].contains("#NOTES:")) {
                 notestr = ""; // first add all the stuff after #NOTES into one string
@@ -191,10 +230,9 @@ public class Simfile {
                 }
                 noteslist = Arrays.asList(notestr.split(",")); // then split by commas
                 Pattern p = Pattern.compile(".{1,4}");
-                for (int b = 0; b < noteslist.size(); b++) // now there is an array of strings of numbers with newlines 
-                {
+                for (int b = 0; b < noteslist.size(); b++) { // now there is an array of strings of numbers with newlines 
                     m = p.matcher(noteslist.get(b)); // use regular expressions to find the notes
-                    note.add(new ArrayList < > ());
+                    note.add(new ArrayList<>());
                     while (m.find()) {
                         note.get(b).add(m.group()); // split them into an array inside the other array for each measure
                     }
@@ -210,7 +248,7 @@ public class Simfile {
      */
     public int NoteCount() {
         int count = 0;
-        for (List < String > sub: this.Notes()) {
+        for (List<String> sub: this.Notes()) {
             count += sub.size();
         }
 
@@ -223,7 +261,7 @@ public class Simfile {
      * The strings are the same as the Notes() function for arrows, and the times are doubles converted into String objects.
      * If there are no notes in the simfile, a blank array will be returned.
      */
-    public List < List < String >> NotesTime() {
+    public List<List<String>> NotesTime() {
         offset = this.Offset();
         note1 = this.Notes();
 
@@ -234,7 +272,7 @@ public class Simfile {
         double currentBPS = currentBPM / 60;
         double currentBPuS = currentBPS / 1000000;
 
-        List < List < String >> notestime = new ArrayList < List < String >> ();
+        List<List<String>> notestime = new ArrayList<List<String>>();
 
         double ts = offset + globaloffset;
 
@@ -242,7 +280,7 @@ public class Simfile {
         {
             for (int a = 0; a < note1.get(i).size(); a++) // for each note in a beat
             {
-                List < String > tmp = new ArrayList < String > (); // make a temporary list with the current time and note
+                List<String> tmp = new ArrayList<String>(); // make a temporary list with the current time and note
                 tmp.add(Double.toString(ts));
                 tmp.add(note1.get(i).get(a));
                 notestime.add(tmp); // add this list to the main list
